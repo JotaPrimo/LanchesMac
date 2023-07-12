70bdfa4dbb9786ba454d933a682c7c8fa979e582
sistemasimcg <sistemas@institutomirim.org.br>
2023-07-12 14:22:47 -0300

    first commit

---
+++ LanchesMac/Program.cs
@@ -0,0 +1,18 @@
+
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore;
+
+
+namespace LanchesMac;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        CreateWebHostBuilder(args).Build().Run();
+    }
+
+    public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+        WebHost.CreateDefaultBuilder(args)
+            .UseStartup<Startup>();
+}
