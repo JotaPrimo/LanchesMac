f507b179e30e854f63b5724cc7de709439f5fe55
JotaPrimo <gestald118@gmail.com>
2023-07-15 12:47:20 -0400

    Populando a tabela Categorias com dados iniciais

--- LanchesMac/Context/AppDbContext.cs
+++ LanchesMac/Context/AppDbContext.cs
@@ -13,5 +13,60 @@ namespace LanchesMac.Context
         public DbSet<Categoria> Categorias { get; set; }
         public DbSet<Lanche> Lanches { get; set; }
 
+        public void SeedData()
+        {
+            if (!Categorias.Any())
+            {
+                var categorias = new Categoria[]
+                {
+                    new Categoria { Id = 2, CategoriaNome = "Saudavel ", Descricao = "Excepteur sint occaecat cupidatat non proident"},
+                    new Categoria { Id = 3, CategoriaNome = "Organico", Descricao = "Sed ut perspiciatis unde omnis iste natus error"},
+                    new Categoria { Id = 4, CategoriaNome = "Fora da Academia", Descricao = "Nemo enim ipsam voluptatem quia voluptas"},
+                };
+
+                Categorias.AddRange(categorias);
+                SaveChanges();
+            }
+
+            if (!Lanches.Any())
+            {               
+
+                // Popula a tabela de lanches com a categoria correta
+                var lanches = new Lanche[]
+                {
+                    new Lanche
+            {
+                Nome = "Lanche 1",
+                DescricaoCurta = "Descrição curta do Lanche 1",
+                DescricaoDetalhada = "Descrição detalhada do Lanche 1",
+                Preco = 10.99,
+                ImagemUrl = "url-da-imagem-lanche-1",
+                ImagemThumbnailUrl = "url-thumbnail-da-imagem-lanche-1",
+                IslanchePreferido = true,
+                EmEstoque = true,
+                CategoriaId = 1,
+                DataDeCriacao = DateTime.Now
+            },
+            new Lanche
+            {
+                Nome = "Lanche 2",
+                DescricaoCurta = "Descrição curta do Lanche 2",
+                DescricaoDetalhada = "Descrição detalhada do Lanche 2",
+                Preco = 8.99,
+                ImagemUrl = "url-da-imagem-lanche-2",
+                ImagemThumbnailUrl = "url-thumbnail-da-imagem-lanche-2",
+                IslanchePreferido = false,
+                EmEstoque = true,
+                CategoriaId = 2,
+                DataDeCriacao = DateTime.Now
+            },
+
+                };
+
+                Lanches.AddRange(lanches);
+                SaveChanges();
+            }
+        }
+
     }
 }
--- LanchesMac/Startup.cs
+++ LanchesMac/Startup.cs
@@ -25,10 +25,11 @@ public class Startup
     }
 
     // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-    public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+    public void Configure(IApplicationBuilder app, IWebHostEnvironment env, AppDbContext dbContext)
     {
         if (env.IsDevelopment())
         {
+            dbContext.SeedData();
             app.UseDeveloperExceptionPage();
         }
         else
