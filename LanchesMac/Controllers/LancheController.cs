0745cfa68802e0f3f08d62bad825bddb414a1ed2
JotaPrimo <gestald118@gmail.com>
2023-07-16 20:00:20 -0400

    Implementando a view model LancheListViewModel

--- LanchesMac/Controllers/LancheController.cs
+++ LanchesMac/Controllers/LancheController.cs
@@ -1,4 +1,5 @@
 ﻿using LanchesMac.Repositories.Interfaces;
+using LanchesMac.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 
 namespace LanchesMac.Controllers
@@ -14,10 +15,13 @@ namespace LanchesMac.Controllers
 
         public IActionResult List()
         {
-            var lanches = _repository.GetAll;
-            
+            // var lanches = _repository.GetAll;
 
-            return View(lanches);
+            var lancheListViewModel = new LancheListViewModel();
+            lancheListViewModel.Lanches = _repository.GetAll;
+            lancheListViewModel.CategoriaAtual = "Categoria Atual";
+
+            return View(lancheListViewModel);
         }
 
     }
