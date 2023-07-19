75a08db7bad99f6f6213f53fa40411fb10d9e292
JotaPrimo <gestald118@gmail.com>
2023-07-18 20:12:51 -0400

    Criando o Carrinho de Compras

---
+++ LanchesMac/Models/CarrinhoCompra.cs
@@ -0,0 +1,42 @@
+﻿using LanchesMac.Context;
+
+namespace LanchesMac.Models
+{
+    public class CarrinhoCompra
+    {
+        private readonly AppDbContext _appDbContext;
+
+        public CarrinhoCompra(AppDbContext appDbContext)
+        {
+            _appDbContext = appDbContext;
+        }
+
+        public string CarrinhoCompraId { get; set; }
+
+        public List<CarrinhoCompraItem> CarrinhoCompraItems { get; set; }
+
+        public static CarrinhoCompra GetCarrinho(IServiceProvider services)
+        {
+            // definindo uma sessão
+            ISession session =
+                services.GetRequiredService<IHttpContextAccessor>()?.HttpContext.Session;
+
+            // pegando um serviço do tipo do contexto
+            var context = services.GetService<AppDbContext>();
+
+
+            // pegando id do carrinho
+            string carrinhoId = session.GetString("CarrinhoId") ?? Guid.NewGuid().ToString();
+
+            // setando id do carrinho na session
+            session.SetString("CarrinhoId", carrinhoId);
+
+            return new CarrinhoCompra(context)
+            {
+                CarrinhoCompraId = carrinhoId
+            };
+
+        }
+
+    }
+}
