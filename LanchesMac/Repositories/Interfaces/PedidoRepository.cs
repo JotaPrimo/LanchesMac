fa11306acf5ca8ae739c9d94e2ea994ee6d7dc84
sistemasimcg <sistemas@institutomirim.org.br>
2023-08-01 13:25:59 -0300

    67. Localizando lanches por nome - Search

---
+++ LanchesMac/Repositories/Interfaces/PedidoRepository.cs
@@ -0,0 +1,25 @@
+﻿using LanchesMac.Context;
+using LanchesMac.Models;
+
+namespace LanchesMac.Repositories.Interfaces
+{
+    public class PedidoRepository : IPedidoRepository
+    {
+
+        private readonly AppDbContext _appDbContext;
+        private readonly CarrinhoCompra _carrinhoCompra;
+
+        public PedidoRepository(AppDbContext appDbContext, CarrinhoCompra carrinhoCompra)
+        {
+            _appDbContext = appDbContext;
+            _carrinhoCompra = carrinhoCompra;
+        }
+
+        public void CriarPedido(Pedido pedido)
+        {
+            pedido.PedidoEnviado = DateTime.Now;
+        }
+
+
+    }
+}
