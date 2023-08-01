8918980f7867d3ee35740dadccb370d9a27aa53d
JotaPrimo <gestald118@gmail.com>
2023-08-01 07:00:01 -0400

    62. Criar View Componente para exibir as categorias no menu Lanches

---
+++ LanchesMac/Controllers/CategoriaMenucs.cs
@@ -0,0 +1,28 @@
+﻿using LanchesMac.Repositories.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LanchesMac.Controllers
+{
+    public class CategoriaMenucs : ViewComponent
+    {
+        private readonly ICategoriaRepository _categoriaRepository;
+
+        public CategoriaMenucs(ICategoriaRepository categoriaRepository)
+        {
+            _categoriaRepository = categoriaRepository;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            var categorias = _categoriaRepository.GetAll.OrderBy(c => c.CategoriaNome);
+
+            return View(categorias);
+        }
+
+
+    }
+
+
+
+
+}
